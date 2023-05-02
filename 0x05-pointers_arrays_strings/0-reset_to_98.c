c70b9e812d41a7560763b04a40b8e573106f417b
Imane Elaceri <elaceriimane@gmail.com>
2023-05-02 02:11:02 -0700

    task: 98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ *		 and updates the value it points to to 98
+ * @n: the integer as parameter
+ * Owned by: Imane
+ * Return: void
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repository contains my work for the 0x05. C - Pointers, arrays and strings project.
