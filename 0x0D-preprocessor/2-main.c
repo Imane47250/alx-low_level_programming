05a13eaf92317d4057c08b242bb411349a8f93fd
Imane Elaceri <elaceriimane@gmail.com>
2023-05-22 12:59:56 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ * Owned by: Imane
+ * Return: void
+ */
+
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
