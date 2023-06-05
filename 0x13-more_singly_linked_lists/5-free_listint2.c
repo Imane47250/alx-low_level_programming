194306bad859a68f30f8983d19e9bbcab399f32c
Imane47250 <elaceriimane@gmail.com>
2023-06-05 11:25:06 -0700

    mandatory tasks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list, and sets head to NULL
+ * @head: linked list
+ Owned by: Imane
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = ptr->next;
+		free(ptr);
+	}
+
+}
